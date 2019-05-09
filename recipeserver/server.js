274eebcf8759c3d70eadaa8b2b1497807248ae97
unknown <your_email@youremail.com>
2019-05-09 14:19:12 -0700

    server changes

--- recipeserver/server.js
+++ recipeserver/server.js
@@ -52,7 +52,7 @@ app.use(bodyParser.json());
 app.use(cors());
 
 app.get('/', (req, res)=> {
-	res.send(database.inventory);
+	res.send('it is working');
 })
 
 app.post('/signin', (req, res) => { signin.handleSignin(req, res, db, bcrypt)})
