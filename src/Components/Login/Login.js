d4aa9ebec3b6cf412f7f005846476ba144eeeae2
unknown <your_email@youremail.com>
2019-05-11 15:39:36 -0700

    login changes

--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -20,6 +20,7 @@ class Login extends React.Component {
 	}
 
 	onSubmitLogin = (event) => {
+		event.preventDefault();
 		fetch('https://gentle-fortress-56351.herokuapp.com/signin', {
 			method: 'post',
 			headers: {'Content-Type': 'application/json'},
